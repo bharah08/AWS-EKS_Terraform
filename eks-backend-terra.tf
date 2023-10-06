5820594178d1a3d811730471910b7ab5094fc2d1
bharah08 <111727649+bharah08@users.noreply.github.com>
2023-10-06 16:33:13 +0530

    Update eks-backend-terra.tf

--- eks-backend-terra.tf
+++ eks-backend-terra.tf
@@ -6,3 +6,13 @@ terraform {
     dynamodb_table = "state-file"
   }
 }
+resource "aws_dynamodb_table" "terraform_lock" {
+  name           = "statefile"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "lockid"
+
+  attribute {
+    name = "lockid"
+    type = "S"
+  }
+}
