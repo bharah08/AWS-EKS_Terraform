8bf1aba849a2d62e7b3330d78805977fc4254816
bharah08 <111727649+bharah08@users.noreply.github.com>
2023-10-06 16:12:52 +0530

    Update eks-backend-terra.tf

--- eks-backend-terra.tf
+++ eks-backend-terra.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraformstatefile12"
+    bucket = "terraformstatefile1234"
     key    = "terraform.tfstate"
     region = "ap-south-1"
     dynamodb_table = "state-file"
